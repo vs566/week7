c97dd51ce34d0e5351c15d0495e4292bc6597976
MilesZhao <mileszhao1990@yahoo.com>
2017-10-19 09:22:54 -0400

    Add home page link

--- errors.php
+++ errors.php
@@ -1,3 +1,12 @@
 <?php
 echo "<p>We do not have this selection choice, please check again!</p>";
 ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<a href="https://web.njit.edu/~yz746/week7/index.php">Go Back to Home Page</a>
+</body>
+</html>
